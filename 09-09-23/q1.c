0e5751f4c954fa5e21cd27ff250fe84b55390d59
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-09-07 00:21:48 +0530

    Assignment 1

--- 09-09-23/q1.c
+++ 09-09-23/q1.c
@@ -1,98 +1,3 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <unistd.h>
-//
-//#define MAX_SIZE 100
-//
-//void push(int value) {
-//    FILE *file = fopen("stack.txt", "a");
-//    if (file == NULL) {
-//        perror("Error opening file");
-//        exit(1);
-//    }
-//    fprintf(file, "%d\n", value);
-//    fclose(file);
-//}
-//
-//int pop() {
-//    int value;
-//    FILE *file = fopen("stack.txt", "r");
-//    if (file == NULL) {
-//        perror("Error opening file");
-//        exit(1);
-//    }
-//
-//    // Read the last value from the file
-//    int prev_value;
-//    while (fscanf(file, "%d", &value) != EOF) {
-//        prev_value = value;
-//    }
-//
-//    fclose(file);
-//
-//    // Remove the last value from the file
-//    FILE *temp_file = fopen("temp_stack.txt", "w");
-//    if (temp_file == NULL) {
-//        perror("Error opening file");
-//        exit(1);
-//    }
-//
-//    file = fopen("stack.txt", "r");
-//    if (file == NULL) {
-//        perror("Error opening file");
-//        exit(1);
-//    }
-//
-//    while (fscanf(file, "%d", &value) != EOF) {
-//        if (value != prev_value) {
-//            fprintf(temp_file, "%d\n", value);
-//        }
-//    }
-//
-//    fclose(file);
-//    fclose(temp_file);
-//
-//    remove("stack.txt");
-//    rename("temp_stack.txt", "stack.txt");
-//
-//    return prev_value;
-//}
-//
-//int main() {
-//    int choice, value;
-//
-//    while (1) {
-//        printf("1. Push\n");
-//        printf("2. Pop\n");
-//        printf("3. Quit\n");
-//        printf("Enter your choice: ");
-//        scanf("%d", &choice);
-//
-//        switch (choice) {
-//            case 1:
-//                printf("Enter value to push: ");
-//                scanf("%d", &value);
-//                push(value);
-//                printf("%d pushed to stack.\n", value);
-//                break;
-//            case 2:
-//                if (access("stack.txt", F_OK) == -1) {
-//                    printf("Stack is empty.\n");
-//                } else {
-//                    value = pop();
-//                    printf("%d popped from stack.\n", value);
-//                }
-//                break;
-//            case 3:
-//                exit(0);
-//            default:
-//                printf("Invalid choice. Please select again.\n");
-//        }
-//    }
-//
-//    return 0;
-//}
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -129,7 +34,7 @@ int pop() {
         exit(1);
     }
 
-    off_t offset = size - 2; // Move back to the last number's beginning
+    off_t offset = size - 2;
     while (offset >= 0) {
         lseek(fd, offset, SEEK_SET);
         if (read(fd, &value, sizeof(int)) != sizeof(int)) {
@@ -138,13 +43,13 @@ int pop() {
             exit(1);
         }
         if (value == '\n') {
-            offset -= 2; // Move back by one more to skip newline character
+            offset -= 2;
             continue;
         }
         break;
     }
 
-    ftruncate(fd, offset); // Remove the last number and newline character
+    ftruncate(fd, offset);
 
     close(fd);
     return value;
