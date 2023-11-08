214d039a8381739baf45198d4ca1bfa55274b379
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2023-11-08 20:07:54 +0530

    Assignment 5,6

--- 2023-10-11/5/q1.c
+++ 2023-10-11/5/q1.c
@@ -1,4 +1,12 @@
-// Q1
+/*
+ * Q1 : 
+ * Write a program to handle different types of SIGNALS using the same handler function. 
+ * In this program the process will receive different types of signals from system 
+ * The process needs to handle SIGINT, SIGHUP, SIGTERM
+ * For every interrupt the same handler should inform the type of SIGNAL occurred. 
+ * The process need to keep running until it encounters SIGQUIT.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
